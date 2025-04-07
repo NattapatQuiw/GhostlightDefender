8c63a412f4d824853ec87a807248a8ee621402de
NattapatQuiw <nattapat.suree@gmail.com>
2025-04-08 00:27:26 +0700

    Add code timecountdown
    add new script that to control time
    to count it down and when it 0 win scene shown.

---
+++ GhostlightDefenderAR/Assets/Script/TimeCountDown.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class TimeCountDown : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private UiManager uiManager;
+    [SerializeField] private float timeInSeconds = 300f;
+
+    private bool isRunning = false;
+    private float remainingTime;
+    private Coroutine countdownCoroutine;
+
+    public void StartCountdown()
+    {
+        if (!isRunning)
+        {
+            isRunning = true;
+            if(remainingTime <= 0f || remainingTime > timeInSeconds)
+                remainingTime = timeInSeconds;
+
+            countdownCoroutine = StartCoroutine(CountdownRoutine());
+        }
+    }
+
+    private IEnumerator CountdownRoutine()
+    {
+        while (remainingTime > 0 && isRunning)
+        {
+            UpdateTimerDisplay(remainingTime);
+            yield return new WaitForSeconds(1f);
+            remainingTime--;
+        }
+
+        UpdateTimerDisplay(0);
+
+        if (remainingTime <= 0)
+        {
+            uiManager.onGameWinShown();
+        }
+        isRunning = false;
+    }
+
+    private void UpdateTimerDisplay(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+        if(countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+            countdownCoroutine = null;
+        }
+    }
+
+    public void ResetTimer()
+    {
+        StopTimer();
+    remainingTime = timeInSeconds;
+UpdateTimerDisplay(remainingTime);
+    }
+}
