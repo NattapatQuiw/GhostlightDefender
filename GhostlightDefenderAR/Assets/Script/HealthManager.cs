2cbae1aef437d1a59fb8160a2c026860fbcabe5e
NattapatQuiw <nattapat.suree@gmail.com>
2025-04-07 17:36:43 +0700

    HealthSystem Takedamage HealthUpdate
    Added code that shows blood when Take damaged. Ui blood will change. And when pressing reset game, blood will be full as usual.

---
+++ GhostlightDefenderAR/Assets/Script/HealthManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+    [SerializeField] private TMP_Text healthText; 
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthText();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage();
+        }
+    }
+
+    public void takeDamage()
+    {
+        currentHealth--;
+        if (currentHealth < 0) 
+        currentHealth = 0;
+
+        UpdateHealthText();
+    }
+
+    private void UpdateHealthText()
+    {
+        healthText.text = currentHealth.ToString();
+    }
+
+    public void resetHealth()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthText();
+    }
+}
